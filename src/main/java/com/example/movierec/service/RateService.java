6a579430085866b51028b0c26200ef93e63462d7
zhjyz2 <83515581+zhjyz2@users.noreply.github.com>
2023-11-13 22:03:22 +0800

    实现RateController中getRating和getAllRatings

--- src/main/java/com/example/movierec/service/RateService.java
+++ src/main/java/com/example/movierec/service/RateService.java
@@ -1,14 +1,19 @@
 package com.example.movierec.service;
 
 import org.springframework.stereotype.Service;
+import com.example.movierec.entity.Rating;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public interface RateService {
     /**
-     *  评价电影
-     * @param userId 用户id
+     * 评价电影
+     *
+     * @param userId  用户id
      * @param movieId 电影id
-     * @param score 分数
+     * @param score   分数
      * @param comment 评价内容
      * @return 评价结果
      */
@@ -16,8 +21,13 @@ public interface RateService {
 
     /**
      * 点赞电影点评
+     *
      * @param ratingId 评论ID
      * @return 评论结果
      */
     Boolean agreeOnRating(Integer ratingId);
+
+    String selectRating(Integer movieId, Integer userId);
+
+    List<Rating> selectAllRating(Integer movieId);
 }
